71bdf9c38853653591d52d12cf18297f47a6a4cc
irenemonzon <irene.monzonm@gmail.com>
2024-06-30 09:42:18 +1000

    update project with react query and add toast with msj

---
+++ src/components/EditProjectForm.tsx
@@ -0,0 +1,78 @@
+import { Link, useNavigate } from "react-router-dom"
+import ProjectForm from "./ProjectForm"
+import { useForm } from "react-hook-form"
+import { Project, ProjectFormData } from "../types"
+import { useMutation } from "@tanstack/react-query"
+import { updateProject } from "@/services/ProjectApi"
+import { toast } from "react-toastify"
+
+type EditProjectFormProps={
+    data:ProjectFormData
+    projectId:Project['_id']
+}
+
+const EditProjectForm = ({data,projectId}:EditProjectFormProps) => {
+
+    const navigate=useNavigate()
+    
+    const { register,handleSubmit,formState:{errors}}=useForm({defaultValues:{
+        projectName:data.projectName,
+        clientName:data.clientName,
+        description:data.description
+    }})
+    const {mutate}=useMutation({
+        mutationFn:updateProject,
+        onError:(error)=>{
+            toast.error(error.message)
+        },
+        onSuccess:(data)=>{
+            toast.success(data)
+            navigate('/')
+        }
+    })
+
+
+    const handleForm=(formData:ProjectFormData)=>{
+        const data={
+            formData,
+            projectId
+        }
+        mutate(data)
+    }
+
+  return (
+    <>
+    <div className="max-w-3xl mx-auto">
+
+        <h1 className=" text-5xl font-black">Editar proyecto</h1>
+        <p className="text-2xl font-light text-gray-500 mt-5">Llena el siguiente formulario para editar el proyecto</p>
+        <nav className="my-5">
+
+        <Link className="bg-purple-400 hover:bg-purple-500 px-10 py-3 font-bold cursor-pointer  text-white text-xl transition-colors"
+        to='/'
+        >
+            Volver a Proyectos
+        </Link>
+        </nav>
+        <form
+        className="mt-10 bg-white shadow-lg rounded-lg p-10"
+        onSubmit={handleSubmit(handleForm)}
+        noValidate
+        >
+            <ProjectForm 
+                register={register}
+                errors={errors}
+            />
+            <input
+            type="submit"
+            value="Guardar cambios"
+            className="bg-fuchsia-600 hover:bg-fuchsia-700 w-full p-3 text-white uppercase font-bold cursor-pointer transition-colors "
+            />
+
+        </form>
+    </div>
+</>
+  )
+}
+
+export default EditProjectForm
--- src/services/ProjectApi.ts
+++ src/services/ProjectApi.ts
@@ -1,6 +1,6 @@
 import { isAxiosError } from "axios";
 import api from "../lib/axios";
-import { ProjectFormData, dashboardProjectSchema } from "../types";
+import { Project, ProjectFormData, dashboardProjectSchema } from "../types";
 
 export async function createProject(formData:ProjectFormData){
     try{
@@ -21,6 +21,32 @@ export async function getProjects(){
             return response.data
         }
 
+    }catch(error){
+        if(isAxiosError(error)&& error.response){
+            throw new Error(error.response.data.error)
+        }
+    }
+}
+export async function getProjectById(id:Project['_id']){
+    try{
+        const {data}=await api(`/projects/${id}`)
+       return data
+    }catch(error){
+        if(isAxiosError(error)&& error.response){
+            throw new Error(error.response.data.error)
+        }
+    }
+}
+
+type ProjectApiType={
+    formData:ProjectFormData
+    projectId:Project['_id']
+}
+
+export async function updateProject({formData,projectId}:ProjectApiType){
+    try{
+    const {data}=await api.put<string>(`/projects/${projectId}`,formData)
+       return data
     }catch(error){
         if(isAxiosError(error)&& error.response){
             throw new Error(error.response.data.error)
