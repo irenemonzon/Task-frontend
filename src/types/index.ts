cc0d4f8a88964291720aa1d284ac9eeb0f505c34
irenemonzon <irene.monzonm@gmail.com>
2024-07-01 19:13:31 +1000

    create modal to add new task, and create schema to type new task

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,5 +1,21 @@
 import { z } from "zod";
 
+export const taskStatusSchema=z.enum(['pending','onHold','inProgress','underReview',"completed"])
+
+/**tasks */
+export const taskSchema=z.object({
+    _id:z.string(),
+    name:z.string(),
+    description:z.string(),
+    project:z.string(),
+    status:taskStatusSchema
+    
+})
+export type Task=z.infer<typeof taskSchema>
+export type TaskFormData=Pick<Task,'name'|'description'>
+
+
+
 /**projects**/
 
 export const projectSchema=z.object({
