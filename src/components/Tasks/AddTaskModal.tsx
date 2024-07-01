cec9d969a92096fc91a9c6e5ca1ca22b76f55929
irenemonzon <irene.monzonm@gmail.com>
2024-07-01 19:36:11 +1000

    add mutation with react query to create new task

--- src/components/Tasks/AddTaskModal.tsx
+++ src/components/Tasks/AddTaskModal.tsx
@@ -1,9 +1,13 @@
 import { Fragment } from 'react';
 import { Dialog, Transition } from '@headlessui/react';
-import { useLocation, useNavigate } from 'react-router-dom';
+import { useLocation, useNavigate, useParams } from 'react-router-dom';
 import {useForm} from 'react-hook-form'
+import { useMutation } from '@tanstack/react-query';
+
 import TaskForm from './TaskForm';
 import { TaskFormData } from '@/types/index';
+import { createTask } from '@/services/TaskApi';
+import { toast } from 'react-toastify';
 
 export default function AddTaskModal() {
     const navigate=useNavigate()
@@ -12,14 +16,35 @@ export default function AddTaskModal() {
     const modalTask=queryParams.get('newTask')
     const show=modalTask ? true: false
 
+    const params=useParams()
+    const projectId=params.projectId!
+
     const initialValues:TaskFormData={
         name:'',
         description:''
     }
-    const { register,handleSubmit,formState:{errors}}=useForm({defaultValues:initialValues})
+    const { register,handleSubmit,reset,formState:{errors}}=useForm({defaultValues:initialValues})
+
+    const{mutate}=useMutation({
+        mutationFn: createTask,
+        onError:(error)=>{
+            toast.error(error.message)
+
+        },
+        onSuccess:(data)=>{
+            toast.success(data)
+            reset()
+            navigate(location.pathname,{replace:true})
+        }
+
+    })
 
     const handleCreateTask=(formData:TaskFormData)=>{
-        console.log(formData)
+        const data={
+            formData,
+            projectId
+        }
+        mutate(data)
     }
 
 
