6458d7be508b878096e5e39a10e8efdaf6f6aaf5
irenemonzon <irene.monzonm@gmail.com>
2024-07-15 18:44:01 +1000

    create view and integrate with back to recived new token and change password

--- src/views/auth/LoginView.tsx
+++ src/views/auth/LoginView.tsx
@@ -31,9 +31,14 @@ export default function LoginView() {
 
   return (
     <>
+     <h1 className="text-5xl font-black text-white">Iniciar sesion</h1>
+      <p className="text-2xl font-light text-white mt-5">
+       Comienza a planear tus proyectos{''}
+        <span className=" text-fuchsia-500 font-bold"> iniciando sesion en este formulario </span>
+      </p>
       <form
         onSubmit={handleSubmit(handleLogin)}
-        className="space-y-8 p-10 bg-white"
+        className="space-y-8 p-10 mt-10 bg-white"
         noValidate
       >
         <div className="flex flex-col gap-5">
@@ -89,6 +94,12 @@ export default function LoginView() {
         className="text-center text-gray-300 font-normal"
         >
         ¿No tienes cuenta? crear cuenta
+        </Link>
+        <Link 
+        to={'/auth/forgot-password'}
+        className="text-center text-gray-300 font-normal"
+        >
+        ¿Olvidaste tu contraseña? Reestablecer
         </Link>
 
       </nav>
---
+++ src/views/auth/NewPasswordView.tsx
@@ -0,0 +1,26 @@
+import NewPasswordForm from "@/components/auth/NewPasswordForm"
+import NewPasswordToken from "@/components/auth/NewPasswordToken"
+import { ConfirmToken } from "@/types/index"
+import { useState } from "react"
+
+const NewPasswordView = () => {
+  const [token,setToken]=useState<ConfirmToken['token']>("")
+
+  const [isValidToken,setIsValidToken]=useState(false)
+
+  return (
+    <>
+      <h1 className="text-5xl font-black text-white">Reestablecer password</h1>
+      <p className="text-2xl font-light text-white mt-5">
+        Ingresa el codigo que recibiste {''}
+        <span className=" text-fuchsia-500 font-bold"> por email</span>
+      </p>
+
+      {!isValidToken ? 
+      <NewPasswordToken token={token} setToken={setToken} setIsValidToken={setIsValidToken}/> : <NewPasswordForm token={token}/> }
+
+    </>
+  )
+}
+
+export default NewPasswordView
