d7e655c5381a3da676f8510fe4c6798ffd687e32
irenemonzon <irene.monzonm@gmail.com>
2024-08-07 18:05:54 +1000

    connect endpoint to change password and update profile

---
+++ src/services/ProfileApi.ts
@@ -0,0 +1,33 @@
+import api from "../lib/axios";
+import { isAxiosError } from "axios";
+import { UpdateCurrentPasswordForm, UserProfileForm } from "../types";
+
+
+export async function updateProfile(formData:UserProfileForm){
+    try{
+        const{data}=await api.put<string>('/auth/profile',formData)
+        return data
+
+
+    }catch(error){
+        if(isAxiosError(error)&&error.message){
+            throw new Error(error.response?.data.error)
+        }
+    }
+  
+}
+export async function changePassword(formData:UpdateCurrentPasswordForm){
+    try{
+        const{data}=await api.post<string>('/auth/update-password',formData)
+        return data
+
+
+    }catch(error){
+        if(isAxiosError(error)&&error.message){
+            throw new Error(error.response?.data.error)
+        }
+    }
+  
+}
+
+
